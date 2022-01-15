c0dbd8e49cc61b277d6c59de3776273090e18823
arun347 <kanjoorarun@gmail.com>
2022-01-15 21:23:06 +0530

    update: setup corsmiddleware, added:requirements.txt

--- app/main.py
+++ app/main.py
@@ -1,12 +1,20 @@
+import imp
 from fastapi import FastAPI,HTTPException,Depends,APIRouter
 from . import models
 from .database import db_engine
 from .routes import admin,trainer,receptionist,auth
-
+from fastapi.middleware.cors import CORSMiddleware
 models.Base.metadata.create_all(bind=db_engine)
 
 app=FastAPI()
-
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # setup routers
 app.include_router(auth.router)
 app.include_router(admin.router)
